ce227eb9427d61a701f6fd945d2e6d905607095d
Ruben Vazquez <ruvaz@live.com>
2021-03-03 21:15:11 -0600

    base test

---
+++ tests/conftest.py
@@ -0,0 +1,27 @@
+import pytest
+from selenium import webdriver
+import time
+driver = None
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser_name", action="store", default="chrome"
+    )
+
+
+@pytest.fixture(scope="class")
+def setup(request):
+    global driver
+    browser_name=request.config.getoption("browser_name")
+    if browser_name == "chrome":
+        driver = webdriver.Chrome(executable_path="C:\\selenium\\chromedriver.exe")
+    elif browser_name == "firefox":
+        driver = webdriver.Firefox(executable_path="C:\\selenium\\geckodriver.exe")
+    elif browser_name == "IE":
+        print("IE driver")
+    driver.get("file:///C:/automation/site/index.html")
+    driver.maximize_window()
+
+    request.cls.driver = driver
+    yield
+    driver.close()
